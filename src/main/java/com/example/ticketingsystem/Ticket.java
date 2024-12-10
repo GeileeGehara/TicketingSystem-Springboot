c3c1a0e49fbe27737895ce637889853e2a6992b7
Geilee Gehara <geilee.20232530@iit.ac.lk>
2024-12-10 12:30:33 +0530

    handle error

--- src/main/java/com/example/ticketingsystem/Ticket.java
+++ src/main/java/com/example/ticketingsystem/Ticket.java
@@ -3,7 +3,6 @@ package com.example.ticketingsystem;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-
 import java.math.BigDecimal;
 
 @Entity
@@ -13,6 +12,9 @@ public class Ticket {
     private int ticketId;
     private String eventName;
     private BigDecimal ticketPrice;
+
+    public Ticket(){
+    }
 
     public Ticket(String eventName, BigDecimal ticketPrice) {
         this.eventName = eventName;
